d227cf4c4dc75d568e84c18b5a8af39c6c4e9231
Colin Gasiewicz <gasiewca@clarkson.edu>
2024-11-05 17:39:00 -0500

    Part 2.1 - 2.5

---
+++ RSA Project/part2_5.py
@@ -0,0 +1,21 @@
+from time import process_time
+from sympy import nextprime
+import random
+
+bit_lengths = [7, 10, 12, 50, 100, 200, 500, 1000, 2400, 10000]
+
+timing_results = []
+
+
+for bit_length in bit_lengths:
+    start_time = process_time()
+    p = nextprime(random.getrandbits(bit_length))
+    q = nextprime(random.getrandbits(bit_length))
+
+    n = p * q
+    end_time = process_time()
+
+    print({
+        "bit_length": bit_length,
+        "time_taken_seconds": (end_time - start_time),
+    })
