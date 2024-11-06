6a5b6d935b68265cccf2cdd165dcd93bbfc8bb7f
Owen Danke <39525303+owendanke@users.noreply.github.com>
2024-11-06 17:56:47 -0500

    Added section 3 driver

---
+++ Section3.py
@@ -0,0 +1,28 @@
+
+def str_to_int(string):
+    # convert string
+    hex_values = []   # buffer to hold values
+    for char in string:
+        z = ord(char)   # convert char to ascii int
+        hex_values.append(str(f'{z:x}'))   # add hex to buffer
+    return ''.join(hex_values)   # join all elements and return
+
+def int_to_str(num):
+    # Convert ascii values back to characters
+    in_str = str(num)   # convert input to string
+    string = ""   # string buffer to return
+    for i in range(0, len(in_str), 2):
+        z = "0x" + in_str[i] + in_str[i+1]   # create the hex number from pairs
+        z = int(z, 16)   # convert hex to int
+        string += chr(z)   # convert int to char and append to string
+    return string   # return the string
+
+# A quick driver that demonstrates the functionality
+my_str = "abc def"
+my_str_int = str_to_int(my_str)
+
+print(my_str)
+
+print(my_str_int)
+
+print(int_to_str(my_str_int))
